511b67402e99d3356efafb8760f03bc5f3dec4eb
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-18 12:56:29 -0800

    created useEffect functions and movielist component

--- src/WatchListPage.js
+++ src/WatchListPage.js
@@ -3,7 +3,19 @@ import MovieList from './MovieList';
 import { getWatchList } from './services/fetch-utils';
 
 export default function WatchListPage() {
+  const [moviesArr, setMoviesArr] = useState([]);
+
+  useEffect(() => {
+    async function fetch(){
+      const watchlist = getWatchList();
+      setMoviesArr(watchlist);
+    }
+    fetch();
+  }, []);
   return (
-    <div>WatchListPage</div>
+    <div>
+      <h3>My Watchlist</h3>
+      < MovieList moviesArr={moviesArr} setMoviesArr={setMoviesArr} />
+    </div>
   );
 }
