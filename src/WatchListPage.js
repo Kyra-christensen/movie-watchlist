cd940a79696bf885339cd52ed98cba54f809053c
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-18 12:46:06 -0800

    imported files and functions needed for the watchlistpage

---
+++ src/WatchListPage.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import MovieList from './MovieList';
+import { getWatchList } from './services/fetch-utils';
+
+export default function WatchListPage() {
+  return (
+    <div>WatchListPage</div>
+  );
+}
