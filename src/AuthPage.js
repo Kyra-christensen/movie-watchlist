3608569444b16e84d9d704a0ba6b3d31da34e10a
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-18 11:13:26 -0800

    created handle sign in/out functions

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -5,6 +5,24 @@ export default function AuthPage({ setCurrentUser }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  async function handleSignIn(e) {
+    e.preventDefault();
+
+    const user = await signIn(email, password);
+    setCurrentUser(user);
+    setEmail('');
+    setPassword('');
+  }
+
+  async function handleSignUp(e) {
+    e.preventDefault();
+
+    const user = await signUp(email, password);
+    setCurrentUser(user);
+    setEmail('');
+    setPassword('');
+  }
+
   return (
     <div>AuthPage</div>
   );
