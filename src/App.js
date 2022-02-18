de9aa101797c64add5f5b905dd7ce452810a381f
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-18 10:48:35 -0800

    imported all necessary files and functions that will be used

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  NavLink
+} from 'react-router-dom';
+import { useState } from 'react';
+import AuthPage from './AuthPage';
+import SearchPage from './SearchPage';
+import WatchListPage from './WatchListPage';
+import { logout } from './services/fetch-utils'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
   );
 }
 
