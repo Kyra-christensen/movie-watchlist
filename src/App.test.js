640f16163a5ef404458f81117119a334d641e242
Kyra Christensen <christensen.kyral@gmail.com>
2022-02-21 09:15:26 -0800

    fixed testing file, test passes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('render signIn button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const signInButton = screen.getByText(/Sign In/i);
+  expect(signInButton).toBeInTheDocument();
+  fireEvent.click(signInButton);
 });
